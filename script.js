8c8099a1cb1d4d06ad23d6b239316c7af5abcb72
Bighnesh Panda <bighnesh0456@gmail.com>
2023-07-04 17:55:57 +0530

    Update: added basic js for adding task to list

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-code will be uploaded soon.
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    // this will add the tast to list 
+    if(inputBox.value === ''){
+        alert("You must write something ");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+    }
+    inputBox.value = "";
+}
